d0042cd745ab9d51dac1ba3ee07328d0f592de5f
longd-dev <longduong325@gmail.com>
2025-10-30 09:47:47 -0700

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+console.log("Mongo URI: ", ENV.MONGO_URI);
+
+app.listen(ENV.PORT, () => console.log("Server started on port: ", ENV.PORT));
