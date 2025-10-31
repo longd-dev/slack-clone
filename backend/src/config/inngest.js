b99da388496e884ab770ca5c9d86fc11c49eaf5f
longd-dev <longduong325@gmail.com>
2025-10-31 09:00:34 -0700

    fixed an inngest bugg

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -16,12 +16,12 @@ const syncUser = inngest.createFunction(
     const { id, email_addresses, first_name, last_name, image_url } =
       event.data;
 
-    const newUser = await User.create({
+    const newUser = {
       clerkId: id,
       email: email_addresses[0]?.email_addresses,
       name: `${first_name || ""} ${last_name || ""}`,
       image: image_url,
-    });
+    };
 
     await User.create(newUser);
   }
